e698eb3d60d200098dbc56aa780d288e74642e79
Park jihyun <thotemily@naver.com>
2025-05-21 03:53:25 +0900

    chore : 인스타그램 링크 수정

--- src/Perform/components/SchedulePage.jsx
+++ src/Perform/components/SchedulePage.jsx
@@ -15,13 +15,13 @@ import tippsy from '../images/tippsy.jpg';
 
 // 동아리 공연 스케줄 데이터
 export const scheduleData = [
-  { date: '22', day: '목', start: '18:40', end: '20:20', title: '소리마당', description: `폭넓은 음악을 다루는 서울여대 중앙 노래패 밴드!\n서울여대 중앙 노래패 밴드로 위로와 공감의 음악을 합니다.`, imageUrl: sorimadang },
-  { date: '22', day: '목', start: '18:00', end: '20:20', title: 'S.E.L', description: `서울여대 유일무이 중앙 락밴드! S.E.L.\n다채로운 락 사운드로 심장을 울리는 무대를 선사합니다.`, imageUrl: sel },
-  { date: '22', day: '목', start: '18:00', end: '20:20', title: '한혜윰', description: `생각을 춤으로, 춤을 꿈으로!\n서울여대 탈춤 동아리로 50년 역사의 전통을 자랑합니다.`, imageUrl: hyeyum },
-  { date: '23', day: '금', start: '15:00', end: '16:00', title: 'SWURS 응원대제전', description: `서울여대의 꽃, 막힘없이 피어라!\n서울여자대학교 응원단 동아리 SWURS`, imageUrl: swurs },
-  { date: '23', day: '금', start: '16:30', end: '17:30', title: '청천벽력', description: `서울여대 유일무이 중앙 풍물패! 청천벽력`, imageUrl: chug },
-  { date: '23', day: '금', start: '18:00', end: '19:00', title: 'S.A.K.E', description: `서울여대 유일 재즈 댄스 퍼포먼스 동아리! S.A.K.E.`, imageUrl: sake },
-  { date: '23', day: '금', start: '19:30', end: '20:30', title: 'TIPSSY', description: `서울여대 유일 스트릿 댄스 동아리! TIPSSY`, imageUrl: tippsy },
+  { date: '22', day: '목', start: '18:40', end: '20:20', title: '소리마당', description: `폭넓은 음악을 다루는 서울여대 중앙 노래패 밴드!\n서울여대 중앙 노래패 밴드로 위로와 공감의 음악을 합니다.`, imageUrl: sorimadang, linkUrl: 'https://www.instagram.com/sorimadang_swu/' },
+  { date: '22', day: '목', start: '18:00', end: '20:20', title: 'S.E.L', description: `서울여대 유일무이 중앙 락밴드! S.E.L.\n다채로운 락 사운드로 심장을 울리는 무대를 선사합니다.`, imageUrl: sel, linkUrl: 'https://www.instagram.com/s.e.l.swu/' },
+  { date: '22', day: '목', start: '18:00', end: '20:20', title: '한혜윰', description: `생각을 춤으로, 춤을 꿈으로!\n서울여대 탈춤 동아리로 50년 역사의 전통을 자랑합니다.`, imageUrl: hyeyum, linkUrl: 'https://www.instagram.com/hyeyumies/' },
+  { date: '23', day: '금', start: '15:00', end: '16:00', title: 'SWURS 응원대제전', description: `서울여대의 꽃, 막힘없이 피어라!\n서울여자대학교 응원단 동아리 SWURS\n최근에는 키움 히어로즈의 홈 구장인 고척 스카이돔에서 공연 하기도 하였습니다.`, imageUrl: swurs, linkUrl: 'https://www.instagram.com/swurs_cheerteam/' },
+  { date: '23', day: '금', start: '16:30', end: '17:30', title: '청천벽력', description: `서울여대 유일무이 중앙 풍물패! 청천벽력\n다양한 전통악기로 하나의 울림을 만들어내는 풍물패 동아리입니다.`, imageUrl: chug, linkUrl: 'https://www.instagram.com/bolt_from_the_blue.swu/' },
+  { date: '23', day: '금', start: '18:00', end: '19:00', title: 'S.A.K.E', description: `서울여대 유일 재즈 댄스 퍼포먼스 동아리! S.A.K.E.\n재즈댄스에 기반한 창작안무와 K-POP 커버댄스를 선보이는 동아리입니다.`, imageUrl: sake, linkUrl: 'https://www.instagram.com/s.a.k.e._jazz/' },
+  { date: '23', day: '금', start: '19:30', end: '20:30', title: 'TIPSSY', description: `서울여대 유일 스트릿 댄스 동아리! TIPSSY\n다양한 스트릿 장르의 댄스와 K-POP 커버까지 소화하는 댄스 동아리입니다.`, imageUrl: tippsy, linkUrl: 'https://www.instagram.com/tipssy_swu/' },
 ];
 
 // 네비게이션에 표시할 날짜 목록
